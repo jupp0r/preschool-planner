3fc8e8fed6ddc17a4db29b2a6b6dca2cd12f2267
Jupp Müller <jupp0r@gmail.com>
2018-08-12 19:19:02 -0700

    Input reading works

--- src/input.rs
+++ src/input.rs
@@ -3,47 +3,88 @@ use failure::Error;
 #[derive(Debug, Deserialize, PartialOrd, PartialEq)]
 pub struct Record {
     pub id: u64,
+    #[serde(rename = "first preference parent 1")]
     pub first_preference_parent_1: Option<Weekday>,
+    #[serde(rename = "first preference parent 2")]
     pub first_preference_parent_2: Option<Weekday>,
+    #[serde(rename = "first preference parent 3")]
     pub first_preference_parent_3: Option<Weekday>,
+    #[serde(rename = "first preference parent 4")]
+    pub first_preference_parent_4: Option<Weekday>,
+    #[serde(rename = "first preference parent 5")]
+    pub first_preference_parent_5: Option<Weekday>,
+    #[serde(rename = "second preference parent 1")]
     pub second_preference_parent_1: Option<Weekday>,
+    #[serde(rename = "second preference parent 2")]
     pub second_preference_parent_2: Option<Weekday>,
+    #[serde(rename = "second preference parent 3")]
     pub second_preference_parent_3: Option<Weekday>,
+    #[serde(rename = "anti preference parent")]
     pub anti_preference_parent: Option<Weekday>,
+    #[serde(rename = "experienced parent")]
     pub experienced_parent: YesNo,
+    #[serde(rename = "baby room")]
     pub baby_room: YesNo,
+    //    #[serde(rename = "introvert/extrovert")]
     //    pub introvert_extrovert: IntrovertExtrovert,
+    #[serde(rename = "days for first child")]
     pub days_for_first_child: u64,
+    #[serde(rename = "first preference first child 1")]
     pub first_preference_first_child_1: Option<Weekday>,
+    #[serde(rename = "first preference first child 2")]
     pub first_preference_first_child_2: Option<Weekday>,
+    #[serde(rename = "first preference first child 3")]
     pub first_preference_first_child_3: Option<Weekday>,
+    #[serde(rename = "first preference first child 4")]
     pub first_preference_first_child_4: Option<Weekday>,
+    #[serde(rename = "first preference first child 5")]
     pub first_preference_first_child_5: Option<Weekday>,
+    #[serde(rename = "first preference second child 1")]
     pub first_preference_second_child_1: Option<Weekday>,
+    #[serde(rename = "first preference second child 2")]
     pub first_preference_second_child_2: Option<Weekday>,
+    #[serde(rename = "first preference second child 3")]
     pub first_preference_second_child_3: Option<Weekday>,
+    #[serde(rename = "first preference second child 4")]
     pub first_preference_second_child_4: Option<Weekday>,
+    #[serde(rename = "first preference second child 5")]
     pub first_preference_second_child_5: Option<Weekday>,
 
+    #[serde(rename = "days for second child")]
+    pub days_for_second_child: Option<u64>,
+    #[serde(rename = "second preference first child 1")]
     pub second_preference_first_child_1: Option<Weekday>,
+    #[serde(rename = "second preference first child 2")]
     pub second_preference_first_child_2: Option<Weekday>,
+    #[serde(rename = "second preference first child 3")]
     pub second_preference_first_child_3: Option<Weekday>,
+    #[serde(rename = "second preference first child 4")]
     pub second_preference_first_child_4: Option<Weekday>,
+    #[serde(rename = "second preference first child 5")]
     pub second_preference_first_child_5: Option<Weekday>,
+    #[serde(rename = "second preference second child 1")]
     pub second_preference_second_child_1: Option<Weekday>,
+    #[serde(rename = "second preference second child 2")]
     pub second_preference_second_child_2: Option<Weekday>,
+    #[serde(rename = "second preference second child 3")]
     pub second_preference_second_child_3: Option<Weekday>,
+    #[serde(rename = "second preference second child 4")]
     pub second_preference_second_child_4: Option<Weekday>,
+    #[serde(rename = "second preference second child 5")]
     pub second_preference_second_child_5: Option<Weekday>,
 }
 
 pub fn load_from_file(path: &str) -> Result<Vec<Record>, Error> {
     use csv;
     let mut reader = csv::Reader::from_path(&path)?;
-    Ok(reader.deserialize().map(|r| r.unwrap()).collect())
+    reader
+        .deserialize()
+        .map(|r| r.map_err(|e| e.into()))
+        .collect()
 }
 
 #[derive(Debug, Deserialize, PartialOrd, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub enum Weekday {
     Mon,
     Tues,
@@ -53,13 +94,35 @@ pub enum Weekday {
 }
 
 #[derive(Debug, Deserialize, PartialOrd, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub enum YesNo {
     Yes,
     No,
 }
 
 #[derive(Debug, Deserialize, PartialOrd, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub enum IntrovertExtrovert {
     Introvert,
     Extrovert,
+}
+
+impl From<YesNo> for bool {
+    fn from(i: YesNo) -> bool {
+        match i {
+            YesNo::Yes => true,
+            YesNo::No => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let res = load_from_file("./enrollment.csv");
+        assert!(res.is_ok(), "error: {:?}", res)
+    }
 }
--- src/model.rs
+++ src/model.rs
@@ -1,3 +1,6 @@
+use input::{Record, Weekday as InputWeekday};
+use std::convert::From;
+
 #[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
 pub struct Input {
     pub parents: Vec<Parent>,
@@ -6,14 +9,14 @@ pub struct Input {
 #[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
 pub struct Parent {
     pub name: String,
-    pub first_work_preferences: Vec<WeekDay>,
-    pub second_work_preferences: Vec<WeekDay>,
+    pub first_work_preferences: Vec<Weekday>,
+    pub second_work_preferences: Vec<Weekday>,
     pub children: Vec<Child>,
-    pub is_inexperienced: bool,
+    pub is_experienced: bool,
 }
 
 #[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
-pub enum WeekDay {
+pub enum Weekday {
     Monday,
     Tuesday,
     Wednesday,
@@ -23,7 +26,89 @@ pub enum WeekDay {
 
 #[derive(PartialOrd, Ord, PartialEq, Eq, Debug)]
 pub struct Child {
-    first_caredfor_preferences: Vec<WeekDay>,
-    second_caredfor_preferences: Vec<WeekDay>,
-    age: u64,
+    first_caredfor_preferences: Vec<Weekday>,
+    second_caredfor_preferences: Vec<Weekday>,
+    days_in_school: u64,
+}
+
+impl From<Record> for Parent {
+    fn from(record: Record) -> Parent {
+        Parent {
+            name: record.id.to_string(),
+            first_work_preferences: vec![
+                record.first_preference_parent_1,
+                record.first_preference_parent_2,
+                record.first_preference_parent_3,
+            ].into_iter()
+                .filter_map(|e| e.map(|v| v.into()))
+                .collect(),
+            second_work_preferences: vec![
+                record.second_preference_parent_1,
+                record.second_preference_parent_2,
+                record.second_preference_parent_3,
+            ].into_iter()
+                .filter_map(|e| e.map(|v| v.into()))
+                .collect(),
+            is_experienced: record.experienced_parent.into(),
+            children: {
+                let mut ch = vec![Child {
+                    first_caredfor_preferences: vec![
+                        record.first_preference_first_child_1,
+                        record.first_preference_first_child_2,
+                        record.first_preference_first_child_3,
+                        record.first_preference_first_child_4,
+                        record.first_preference_first_child_5,
+                    ].into_iter()
+                        .filter_map(|e| e.map(|v| v.into()))
+                        .collect(),
+                    second_caredfor_preferences: vec![
+                        record.second_preference_first_child_1,
+                        record.second_preference_first_child_2,
+                        record.second_preference_first_child_3,
+                        record.second_preference_first_child_4,
+                        record.second_preference_first_child_5,
+                    ].into_iter()
+                        .filter_map(|e| e.map(|v| v.into()))
+                        .collect(),
+                    days_in_school: record.days_for_first_child,
+                }];
+                if record.days_for_second_child.is_some() {
+                    ch.push(Child {
+                        first_caredfor_preferences: vec![
+                            record.first_preference_second_child_1,
+                            record.first_preference_second_child_2,
+                            record.first_preference_second_child_3,
+                            record.first_preference_second_child_4,
+                            record.first_preference_second_child_5,
+                        ].into_iter()
+                            .filter_map(|e| e.map(|v| v.into()))
+                            .collect(),
+                        second_caredfor_preferences: vec![
+                            record.second_preference_second_child_1,
+                            record.second_preference_second_child_2,
+                            record.second_preference_second_child_3,
+                            record.second_preference_second_child_4,
+                            record.second_preference_second_child_5,
+                        ].into_iter()
+                            .filter_map(|e| e.map(|v| v.into()))
+                            .collect(),
+                        days_in_school: record.days_for_second_child.unwrap(),
+                    });
+                }
+                ch
+            },
+        }
+    }
+}
+
+impl From<InputWeekday> for Weekday {
+    fn from(wd: InputWeekday) -> Weekday {
+        match wd {
+            InputWeekday::Mon => Weekday::Monday,
+            InputWeekday::Tues => Weekday::Tuesday,
+            InputWeekday::Wed => Weekday::Wednesday,
+            InputWeekday::Thurs => Weekday::Thursday,
+            InputWeekday::Fri => Weekday::Friday,
+        }
+    }
 }
